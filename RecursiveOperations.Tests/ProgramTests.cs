a9de18c71d3aeb64fdd625b53ddec145d7528e28
Marius Iulian <marius.ancutescu@outlook.com>
2020-03-19 20:30:38 +0200

    Tests added

--- RecursiveOperations.Tests/ProgramTests.cs
+++ RecursiveOperations.Tests/ProgramTests.cs
@@ -6,9 +6,26 @@ namespace RecursiveOperations.Tests
     public class ProgramsTests
     {
         [Fact]
-        public void Test1()
+        public void ReturnsNAWhenThereAreNoCombinations()
         {
-            
+            Assert.Equal("N/A", Program.GetOperationCombinationsToGivenResult(3, 3));
+        }
+        
+        [Fact]
+        public void ReturnsCorrectResultWhenIsOneCombination()
+        {
+            string result = "1 - 2 + 3 - 4 + 5 = 3\n";
+            Assert.Equal(result, Program.GetOperationCombinationsToGivenResult(5, 3));
+        }
+
+        [Fact]
+        public void ReturnsCorrectResultWhenAreMoreCOmbinations()
+        {
+            string result = "1 + 2 + 3 - 4 - 5 + 6 = 3\n" +
+                "1 + 2 - 3 + 4 + 5 - 6 = 3\n" +
+                "1 - 2 - 3 - 4 + 5 + 6 = 3\n";
+            Assert.Equal(result, Program.GetOperationCombinationsToGivenResult(6, 3));
+                ;
         }
     }
 }
